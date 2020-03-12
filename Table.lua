0f7d7ef31fc9201d03ffefa7fbe3c20216131cb7
RECNEPS-KIPS <2432719656@qq.com>
2020-03-12 15:02:50 +0800

    Lua的table类型

---
+++ Table.lua
@@ -0,0 +1,14 @@
+tab1={} --空表 {}为构造表达式
+
+tab2={k1=100,k2="v2"}
+--两种访问方式
+print(tab2.k1)
+print(tab2["k1"])
+
+--不指定键值,默认为1,2,3,4....
+tab3={"apple","orange","pear"}
+print(tab3[3])
+--table的遍历
+for k,v in pairs(tab3) do
+ print(k.." "..v)
+ end
