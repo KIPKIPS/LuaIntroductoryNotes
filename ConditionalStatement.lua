aac9628ec6b52c20e759c0e131e85f4bae7c376e
RECNEPS-KIPS <2432719656@qq.com>
2020-03-12 14:46:07 +0800

    Lua的string类型

--- ConditionalStatement.lua
+++ ConditionalStatement.lua
@@ -17,11 +17,36 @@ print("true")
 else
 print("false")
 end
-
 --]]
 
+--[[
 a=2e1
 a=2e+1
 a=2e-2
 a=2.3e-8
+print(a)
+--]]
+
+
+html = [[
+<html>
+<head></head>
+<body>
+    <a href="http://www.runoob.com/">²ËÄñ½Ì³Ì</a>
+</body>
+</html>
+]]
+
+print(html)
+print("123".."456")
+print("2" + 6)
+print("2" + "6")
+print("2 + 6")
+print("-2e2" * "6")
+
+str="123456"
+print(#str)
+print(#"123456")
+
+
 print(a)
