d10f1e54fc7dd9ed9d0a2e6eb05920af13fa239e
RECNEPS-KIPS <2432719656@qq.com>
2020-03-12 14:14:00 +0800

    nil的用法

--- DataType.lua
+++ DataType.lua
@@ -1,3 +1,21 @@
+--[[
 print(type("dssd"))
 print(type(13))
 print(type(false))
+print(type(type))
+print(type(print))
+print(type(nil))
+
+--不存在的值返回nil
+print(type(x))
+--]]
+
+tab1 = { key1 = "val1", key2 = "val2", "val3" }
+for k, v in pairs(tab1) do
+    print(k .. " " .. v)
+end
+print()
+tab1.key1 = nil
+for k, v in pairs(tab1) do
+    print(k .. " " .. v)
+end
