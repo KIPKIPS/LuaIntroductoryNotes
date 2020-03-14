df3009ec0aea680c9ff693f0da13b9dae1033d71
RECNEPS-KIPS <2432719656@qq.com>
2020-03-14 14:31:06 +0800

    #取table的长度
    #只能取到索引值的长度,取不到键值对的值

--- Operator.lua
+++ Operator.lua
@@ -10,3 +10,21 @@ print(2^4)
 
 --Âß¼­ÔËËã·û
 -- and or not
+
+
+--[[
+tab={1,2,3,4}
+print(#tab)
+tab[1]=5
+print(#tab)
+tab[1]="asd"
+print(#tab)
+
+--]]
+
+tab={k1="v1",k2="v2"}
+print(#tab)
+
+
+tab={k1="v1",2,"dasf",k2="v2"}
+print(#tab)
