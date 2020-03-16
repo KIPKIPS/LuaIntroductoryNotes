eb78d4b0c4d94ac62a9a9805e7a88c20991da678
RECNEPS-KIPS <2432719656@qq.com>
2020-03-16 19:44:16 +0800

    Lua排序

--- Table.lua
+++ Table.lua
@@ -27,7 +27,7 @@ tab1[10]="v3"
 print(tab1[10])
 --]]
 
-
+--[[
 tab={}
 tab[1]=2
 tab["name"]="wkp"
@@ -63,3 +63,36 @@ end
 for i=1,#tab do
     print(tab[i])
 end
+--]]
+
+tab={"Lua","C#","C++","Python","JavaScript","Java","PHP","C"}
+for index in pairs(tab) do
+    print(tab[index]);
+end
+
+--排序
+print("排序后:")
+table.sort(tab)
+for index in pairs(tab) do
+    print(index,tab[index]);
+end
+
+--字符类型比较大小
+function Max(tab,dataType)
+    local max
+    if dataType=="String" then
+        max=""
+    end
+    if dataType=="Number" then
+        max=tab[1]
+    end
+    for index in pairs(tab) do
+        if max<tab[index] then
+            max=tab[index]
+        end
+    end
+    return max
+end
+num={1,34,5,345,3,2,765,758,234,21,4,432,5,25,3}
+print(Max(tab,"String"))
+print(Max(num,"Number"))
