481ef6fa5aba1cabb83049feca391636a93627de
RECNEPS-KIPS <2432719656@qq.com>
2020-04-28 21:48:38 +0800

    匹配汉字

--- CheckChinese.lua
+++ CheckChinese.lua
@@ -1,18 +1,28 @@
 ---匹配中文字符
------ 以只读方式打开文件
-file = io.open("TestText.lua", "r")
-for line in io.lines("TestText.lua") do
-    print(line)
-end
-function ChineseCheck(s)
+--[[function MatchChinese(s)
     local ret = {};
     local f = '[%z\1-\127\194-\244][\128-\191]*';
     local line, lastLine, isBreak = '', false, false;
     for v in s:gfind(f) do
-        table.insert(ret, {c=v,isChinese=(#v~=1)});
+        table.insert(ret, { c = v, isChinese = (#v ~= 1) });
     end
-    return ret;
+    return ret;`
+end]]
+-----以只读方式打开文件
+-- 以只读方式打开文件
+file = io.open("TestText.lua", "r")
+str=file:read("*a")
+print(str)
+-- 关闭打开的文件
+file:close()
+print('---------------------------------------------------')
+---匹配中文字符串
+function MatchChinese(str)
+    for s in string.gfind(str,'"(.-)"') do
+        if string.match(s,"[%z\1-\127\194-\244][\128-\191]+")~=nil then
+            print(s)
+            print(string.find(str,s))
+        end
+    end
 end
-for k, v in ipairs(CheckChinese('a中fj文b+')) do
-    print(k,v.c,#v.c,v.isChinese);
-end
+MatchChinese(str)
